849ea2730abd7050d2f1a6793ccf86084d0141b0
YashwanthK <yashwanthkothakota@gmail.com>
2025-06-16 12:07:07 +0530

    add: echo builtin

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,20 @@ fn main() {
         let mut input = String::new();
 
         io::stdin().read_line(&mut input).unwrap();
-        let input_value = input.trim();
-        if input_value == "exit 0" {
-            exit(0);
+        let input_command = input.trim();
+        let mut input_command_name = "";
+        let mut input_command_args = "";
+
+        if input_command.split_once(" ").is_some() {
+            (input_command_name, input_command_args) = input_command.split_once(" ").unwrap();
         }
-        println!("{}: command not found", input_value);
+
+        if input_command_name == "exit" {
+            exit(0);
+        } else if input_command_name == "echo" {
+            println!("{}", input_command_args);
+        } else {
+            println!("{}: command not found", input_command);
+        }
     }
 }
